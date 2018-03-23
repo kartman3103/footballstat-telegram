188be90d19bef36b9a5bff36bec28b8db8ffa587
kartman3103 <pasha3103@bk.ru>
2018-03-23 17:28:03 +0300

    add logger

--- src/main/java/telegrambot/controller/HttpController.kt
+++ src/main/java/telegrambot/controller/HttpController.kt
@@ -1,15 +1,19 @@
 import org.apache.http.HttpResponse
 import org.apache.http.client.fluent.Request
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
 class HttpController {
+    val logger = LoggerFactory.getLogger(this.javaClass)
+
     fun sendGet(url : String) : HttpResponse {
         try {
             val request = Request.Get(url)
             return request.execute().returnResponse()
         }
         catch (ex : Exception) {
+            logger.error("Unsuccessfull GET request with url {}", url, ex)
             throw ex
         }
     }
