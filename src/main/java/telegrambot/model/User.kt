fa32cb3343a52da22e052e46d360095e1c1e48d8
kartman3103 <pasha3103@bk.ru>
2018-04-06 18:59:25 +0300

    success parser test

--- src/main/java/telegrambot/model/User.kt
+++ src/main/java/telegrambot/model/User.kt
@@ -1,6 +1,6 @@
 package telegrambot.model
 
-data class User(val id : Int,
+data class User(val id : Long,
                 val isBot : Boolean,
                 val firstName : String,
                 val secondName : String,
--- src/main/java/telegrambot/parsers/ModelParser.kt
+++ src/main/java/telegrambot/parsers/ModelParser.kt
@@ -1,13 +1,20 @@
 package telegrambot.parsers
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.stereotype.Component
 import telegrambot.model.User
 
 @Component
 open class ModelParser {
-    
-
     fun parseUser(json : String) : User {
-        return User(3, false, "", "", "")
+        return with(ObjectMapper().readTree(json)) {
+            User(
+                get("id").longValue(),
+                get("is_bot").booleanValue(),
+                get("first_name").textValue(),
+                get("last_name").textValue(),
+                get("language_code").textValue()
+            )
+        }
     }
 }
